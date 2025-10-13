cbaefc80252f72f4df19d675c32fec9edac45acc
hizir <rekoporo0@gmail.com>
2025-10-13 09:54:56 +0300

    new_code

--- first_file1.py
+++ first_file1.py
@@ -2,7 +2,8 @@ def main():
 
     for i in range(10):
        for j in range(10):
-          print(i * j)
+          for f in range(10):
+          print(i * j * f)
 
 if __name__ == "__main__":
     main()
