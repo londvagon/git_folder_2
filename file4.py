8a105b6b1234b8e4a706e3ac6798b9fed9aec01a
hizir <rekoporo0@gmail.com>
2025-10-18 21:08:55 +0300

    two cycle

---
+++ file4.py
@@ -0,0 +1,11 @@
+def three_cycle_func():
+
+    for i in range(10):
+        for j in range(10):
+            print(f"{i * j} \t")
+           
+
+var1 = three_cycle_func()
+var2 = three_cycle_func()
+var3 = three_cycle_func()
+print(var1, var2, var3)
